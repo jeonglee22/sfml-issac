106369170fd7496ad45131767a16ed6d8bd0cab4
김주홍 <gicente1@nate.com>
2025-07-24 10:00:59 +0900

    fix include

--- sfml-project/SceneGame.cpp
+++ sfml-project/SceneGame.cpp
@@ -2,6 +2,11 @@
 #include "SceneGame.h"
 #include "rapidcsv.h"
 #include "SpriteGo.h"
+#include "Isaac.h"
+#include "Tears.h"
+#include "Monster.h"
+#include "Fly.h"
+#include "Spider.h"
 
 SceneGame::SceneGame()
 	: Scene(SceneIds::Stage)
@@ -16,6 +21,42 @@ void SceneGame::Init()
 	texIds.push_back("graphics/background/depths.png");
 	texIds.push_back("graphics/obstacles/grid_spikes.png");
 	texIds.push_back("graphics/obstacles/rocks/rocks_basement.png");
+	texIds.push_back("graphics/character_001_isaac.png");
+	texIds.push_back("graphics/only_tears.png");
+	texIds.push_back("graphics/monster_010_fly.png");
+	texIds.push_back("graphics/temp_background.png");
+	texIds.push_back("graphics/monster_214_level2spider_small.png");
+	fontIds.push_back("fonts/DS-DIGIT.ttf");
+
+	ANI_CLIP_MGR.Load("animations/idle.csv");
+	ANI_CLIP_MGR.Load("animations/run.csv");
+	ANI_CLIP_MGR.Load("animations/jump.csv");
+	ANI_CLIP_MGR.Load("animations/isaac_body_idle.csv");
+	ANI_CLIP_MGR.Load("animations/isaac_head_front.csv");
+	ANI_CLIP_MGR.Load("animations/isaac_run_height.csv");
+	ANI_CLIP_MGR.Load("animations/isaac_run_weight.csv");
+	ANI_CLIP_MGR.Load("animations/isaac_head_side.csv");
+	ANI_CLIP_MGR.Load("animations/isaac_head_rare.csv");
+	ANI_CLIP_MGR.Load("animations/isaac_head_front_tears.csv");
+	ANI_CLIP_MGR.Load("animations/isaac_head_side_tears.csv");
+	ANI_CLIP_MGR.Load("animations/isaac_head_rare_tears.csv");
+	ANI_CLIP_MGR.Load("animations/fly.csv");
+	ANI_CLIP_MGR.Load("animations/spider_patrol.csv");
+	ANI_CLIP_MGR.Load("animations/spider_charge.csv");
+	ANI_CLIP_MGR.Load("animations/spider_jump.csv");
+
+	AddGameObject(new Isaac());
+
+
+	auto fly = new Fly();
+	fly->SetPosition({ 200.f,200.f });
+	AddGameObject(fly);
+
+	auto spider = new Spider();
+	spider->SetPosition({ 300.f, 300.f });
+	AddGameObject(spider);
+
+
 
 	Scene::Init();
 }
